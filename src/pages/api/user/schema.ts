5eb83bab19eb256be3ee3c3d7783274798decc3c
Danilo Woznica <danilowoz@gmail.com>
2021-02-10 09:15:34 +0000

    fix(schema api): avoid rewrite headers

--- src/pages/api/user/schema.ts
+++ src/pages/api/user/schema.ts
@@ -3,19 +3,14 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 import { getUserIdFromTokenId } from 'services/firebase.server'
 
-const checkUser = async (token: string, res: NextApiResponse) => {
-  try {
-    const uid = await getUserIdFromTokenId(token as string)
+const checkUser = async (token: string) => {
+  const uid = await getUserIdFromTokenId(token as string)
 
-    if (uid) {
-      return uid
-    }
-
-    return
-  } catch (err) {
-    res.status(400).json({ error: err.message })
-    return
+  if (uid) {
+    return uid
   }
+
+  return false
 }
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -23,70 +18,78 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
    * SAVE
    */
   if (req.method === 'POST') {
-    // Check user
-    const uid = await checkUser(req.body.token, res)
+    try {
+      // Check user
+      const uid = await checkUser(req.body.token)
 
-    // Body
-    const canvas = req.body.canvas
-    const playlistId = req.body.playlistId
-    if (!canvas) {
-      res.status(400).json({ error: 'missing data field' })
+      // Body
+      const canvas = req.body.canvas
+      const playlistId = req.body.playlistId
+
+      if (!canvas) {
+        res.status(400).json({ error: 'missing data field' })
+        return
+      }
+
+      await admin.database().ref(`/playlistsSchema/${playlistId}`).set(canvas)
+      await admin
+        .database()
+        .ref(`/spotifyAccessToken/${uid}/playlists`)
+        .push(playlistId)
+
+      res.status(200).json({ result: 'ok' })
+      return
+    } catch (err) {
+      res.status(400).json({ error: err.message })
       return
     }
+  } else if (req.method === 'GET') {
+    /**
+     * GET
+     */
+    try {
+      // Check user
+      await checkUser(req.query.token as string)
 
-    await admin.database().ref(`/playlistsSchema/${playlistId}`).set(canvas)
-    await admin
-      .database()
-      .ref(`/spotifyAccessToken/${uid}/playlists`)
-      .push(playlistId)
+      // Body
+      const playlistId = req.query.playlistId
+      if (!playlistId) {
+        res.status(400).json({ error: 'missing playlistId field' })
+        return
+      }
 
-    res.status(200).json({ result: 'ok' })
-    return
-  }
-
-  /**
-   * GET
-   */
-  if (req.method === 'GET') {
-    // Check user
-    await checkUser(req.query.token as string, res)
-
-    // Body
-    const playlistId = req.query.playlistId
-    if (!playlistId) {
-      res.status(400).json({ error: 'missing playlistId field' })
+      admin
+        .database()
+        .ref(`/playlistsSchema/${playlistId}`)
+        .on('value', async (snap) => {
+          res.status(200).json({ result: snap.val() })
+        })
+    } catch (err) {
+      res.status(400).json({ error: err.message })
       return
     }
+  } else if (req.method === 'DELETE') {
+    /**
+     * DELETE
+     */
+    try {
+      // Check user
+      await checkUser(req.body.token)
 
-    admin
-      .database()
-      .ref(`/playlistsSchema/${playlistId}`)
-      .on('value', async (snap) => {
-        res.status(200).json({ result: snap.val() })
-      })
+      // Body
+      const playlistId = req.query.playlistId
+      if (!playlistId) {
+        res.status(400).json({ error: 'missing playlistId field' })
+        return
+      }
 
-    return
-  }
-
-  /**
-   * DELETE
-   */
-  if (req.method === 'DELETE') {
-    // Check user
-    await checkUser(req.query.token as string, res)
-
-    // Body
-    const playlistId = req.query.playlistId
-    if (!playlistId) {
-      res.status(400).json({ error: 'missing playlistId field' })
+      admin.database().ref(`/playlistsSchema/${playlistId}`).remove()
+      res.status(200).json({ result: 'ok' })
+    } catch (err) {
+      res.status(400).json({ error: err.message })
       return
     }
-
-    admin.database().ref(`/playlistsSchema/${playlistId}`).remove()
-    res.status(200).json({ result: 'ok' })
-
-    return
+  } else {
+    res.status(400).json({ error: 'method not supported' })
   }
-
-  res.status(400).json({ error: 'method not supported' })
 }
