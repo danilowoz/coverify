70646e402f2b39dd448b20222634a8794a6c93ba
Danilo Woznica <danilowoz@gmail.com>
2021-03-02 11:14:41 +0000

    fix(seo): force updating the image share

--- src/modules/SEO/SEO.tsx
+++ src/modules/SEO/SEO.tsx
@@ -6,23 +6,25 @@ import { APP_NAME, DESCRIPTION, SHORT_DESCRIPTION, URL } from 'common/constants'
 import faviconSrc from './assets/favicon.png'
 
 const SEO = () => {
+  const imageShare = `${URL}static/share-v2.png`
+
   return (
     <Head>
       <link rel="shortcut icon" href={faviconSrc} />
 
       <title>{`${APP_NAME} - ${SHORT_DESCRIPTION}`}</title>
       <meta name="description" content={DESCRIPTION} />
-      <meta name="image" content={`${URL}static/share.png`} />
+      <meta name="image" content={imageShare} />
 
       <meta property="og:url" content={URL} />
       <meta property="og:title" content={APP_NAME} />
       <meta property="og:description" content={DESCRIPTION} />
-      <meta property="og:image" content={`${URL}static/share.png`} />
+      <meta property="og:image" content={imageShare} />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={APP_NAME} />
       <meta name="twitter:description" content={DESCRIPTION} />
-      <meta name="twitter:image" content={`${URL}static/share.png`} />
+      <meta name="twitter:image" content={imageShare} />
     </Head>
   )
 }
